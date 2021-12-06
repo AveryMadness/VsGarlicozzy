7bd3bd738794a969482830d99bf23ed58c2b428d
AveryMadness <averyfd@shaw.ca>
2021-12-06 10:02:45 -0700

    Madness Engine Shit
    starting to add and comment out madness engine shit for the eventual seperation

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -29,8 +29,8 @@ using StringTools;
 
 // TO DO: Redo the menu creation system for not being as dumb
 class OptionsState extends MusicBeatState
-{
-	var options:Array<String> = ['Garlicozzy', 'Notes', 'Controls', 'Preferences'];
+{				    // custom mod option shit is commented out until i seperate from garlicozzy
+	var options:Array<String> = [/*'Mod Name'*/'Garlicozzy', 'Notes', 'Controls', 'Preferences'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
@@ -101,6 +101,8 @@ class OptionsState extends MusicBeatState
 				
 				case 'Garlicozzy':
 					openSubState(new GarlicozzyOptionsSubstate());
+			      /*case 'Mod Name':
+					openSubState(new ModNameOptionsSubState()); */
 			}
 		}
 	}
@@ -1429,4 +1431,309 @@ class GarlicozzyOptionsSubstate extends MusicBeatSubstate
 		}
 		return options[num] == null || options[num].length < 1;
 	}
+		
+/* class ModNameOptionsSubstate extends MusicBeatSubstate
+{
+	private static var curSelected:Int = 0;
+	static var unselectableOptions:Array<String> = [
+		'Mod Category',
+		''
+	];
+	static var noCheckbox:Array<String> = [
+		'',
+		''
+	];
+
+	static var options:Array<String> = [
+		'Mod Category'
+		'Mod Option 1',
+		'Mod Option 2',
+		'Mod Option 3'
+	];
+
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private var checkboxArray:Array<CheckboxThingie> = [];
+	private var checkboxNumber:Array<Int> = [];
+	private var grpTexts:FlxTypedGroup<AttachedText>;
+	private var textNumber:Array<Int> = [];
+
+	private var showCharacter:Character = null;
+	private var descText:FlxText;
+
+	public function new()
+	{
+		super();
+		// avoids lagspikes while scrolling through menus!
+		showCharacter = new Character(840, 170, 'bf', true);
+		showCharacter.setGraphicSize(Std.int(showCharacter.width * 0.8));
+		showCharacter.updateHitbox();
+		showCharacter.dance();
+		add(showCharacter);
+		showCharacter.visible = false;
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		grpTexts = new FlxTypedGroup<AttachedText>();
+		add(grpTexts);
+
+		for (i in 0...options.length)
+		{
+			var isCentered:Bool = unselectableCheck(i);
+			var optionText:Alphabet = new Alphabet(0, 70 * i, options[i], false, false);
+			optionText.isMenuItem = true;
+			if(isCentered) {
+				optionText.screenCenter(X);
+				optionText.forceX = optionText.x;
+			} else {
+				optionText.x += 300;
+				optionText.forceX = 300;
+			}
+			optionText.yMult = 90;
+			optionText.targetY = i;
+			grpOptions.add(optionText);
+
+			if(!isCentered) {
+				var useCheckbox:Bool = true;
+				for (j in 0...noCheckbox.length) {
+					if(options[i] == noCheckbox[j]) {
+						useCheckbox = false;
+						break;
+					}
+				}
+
+				if(useCheckbox) {
+					var checkbox:CheckboxThingie = new CheckboxThingie(optionText.x - 105, optionText.y, false);
+					checkbox.sprTracker = optionText;
+					checkboxArray.push(checkbox);
+					checkboxNumber.push(i);
+					add(checkbox);
+				} else {
+					var valueText:AttachedText = new AttachedText('0', optionText.width + 80);
+					valueText.sprTracker = optionText;
+					grpTexts.add(valueText);
+					textNumber.push(i);
+				}
+			}
+		}
+
+		descText = new FlxText(50, 600, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		descText.scrollFactor.set();
+		descText.borderSize = 2.4;
+		add(descText);
+
+		for (i in 0...options.length) {
+			if(!unselectableCheck(i)) {
+				curSelected = i;
+				break;
+			}
+		}
+		changeSelection();
+		reloadValues();
+	}
+
+	var nextAccept:Int = 5;
+	var holdTime:Float = 0;
+	override function update(elapsed:Float)
+	{
+		if (controls.UI_UP_P)
+		{
+			changeSelection(-1);
+		}
+		if (controls.UI_DOWN_P)
+		{
+			changeSelection(1);
+		}
+
+		if (controls.BACK) {
+			grpOptions.forEachAlive(function(spr:Alphabet) {
+				spr.alpha = 0;
+			});
+			grpTexts.forEachAlive(function(spr:AttachedText) {
+				spr.alpha = 0;
+			});
+			for (i in 0...checkboxArray.length) {
+				var spr:CheckboxThingie = checkboxArray[i];
+				if(spr != null) {
+					spr.alpha = 0;
+				}
+			}
+			if(showCharacter != null) {
+				showCharacter.alpha = 0;
+			}
+			descText.alpha = 0;
+			close();
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+		}
+
+		var usesCheckbox = true;
+		for (i in 0...noCheckbox.length) {
+			if(options[curSelected] == noCheckbox[i]) {
+				usesCheckbox = false;
+				break;
+			}
+		}
+
+		if(usesCheckbox) {
+			if(controls.ACCEPT && nextAccept <= 0) {
+				switch(options[curSelected]) {
+
+					case 'ModOption1':
+						ClientPrefs.modoption1 = !ClientPrefs.modoption1;
+					case 'ModOption2':
+						ClientPrefs.modoption2 = !ClientPrefs.modoption2;
+					case 'ModOption3':
+						ClientPrefs.modoption3 = !ClientPrefs.modoption3;
+
+
+				}
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				reloadValues();
+			}
+		} else {
+			if(controls.UI_LEFT || controls.UI_RIGHT) {
+				var add:Int = controls.UI_LEFT ? -1 : 1;
+				if(holdTime > 0.5 || controls.UI_LEFT_P || controls.UI_RIGHT_P)
+				switch(options[curSelected]) {
+					case 'Framerate':
+						ClientPrefs.framerate += add;
+						if(ClientPrefs.framerate < 60) ClientPrefs.framerate = 60;
+						else if(ClientPrefs.framerate > 240) ClientPrefs.framerate = 240;
+
+						if(ClientPrefs.framerate > FlxG.drawFramerate) {
+							FlxG.updateFramerate = ClientPrefs.framerate;
+							FlxG.drawFramerate = ClientPrefs.framerate;
+						} else {
+							FlxG.drawFramerate = ClientPrefs.framerate;
+							FlxG.updateFramerate = ClientPrefs.framerate;
+						}
+					case 'Note Delay':
+						var mult:Int = 1;
+						if(holdTime > 1.5) { //Double speed after 1.5 seconds holding
+							mult = 2;
+						}
+						ClientPrefs.noteOffset += add * mult;
+						if(ClientPrefs.noteOffset < 0) ClientPrefs.noteOffset = 0;
+						else if(ClientPrefs.noteOffset > 500) ClientPrefs.noteOffset = 500;
+				}
+				reloadValues();
+
+				if(holdTime <= 0) FlxG.sound.play(Paths.sound('scrollMenu'));
+				holdTime += elapsed;
+			} else {
+				holdTime = 0;
+			}
+		}
+
+		if(showCharacter != null && showCharacter.animation.curAnim.finished) {
+			showCharacter.dance();
+		}
+
+		if(nextAccept > 0) {
+			nextAccept -= 1;
+		}
+		super.update(elapsed);
+	}
+	
+	function changeSelection(change:Int = 0)
+	{
+		do {
+			curSelected += change;
+			if (curSelected < 0)
+				curSelected = options.length - 1;
+			if (curSelected >= options.length)
+				curSelected = 0;
+		} while(unselectableCheck(curSelected));
+
+		var daText:String = '';
+		switch(options[curSelected]) {
+			case 'ModOption1':
+				daText = "Mod Option 1 Description.";
+			case 'ModOption2':
+				daText = "Mod Option 2 Description.";
+			case 'ModOption3':
+				daText = "Mod Option 3 Description.";
+		}
+		descText.text = daText;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			if(!unselectableCheck(bullShit-1)) {
+				item.alpha = 0.6;
+				if (item.targetY == 0) {
+					item.alpha = 1;
+				}
+
+				for (j in 0...checkboxArray.length) {
+					var tracker:FlxSprite = checkboxArray[j].sprTracker;
+					if(tracker == item) {
+						checkboxArray[j].alpha = item.alpha;
+						break;
+					}
+				}
+			}
+		}
+		for (i in 0...grpTexts.members.length) {
+			var text:AttachedText = grpTexts.members[i];
+			if(text != null) {
+				text.alpha = 0.6;
+				if(textNumber[i] == curSelected) {
+					text.alpha = 1;
+				}
+			}
+		}
+
+		showCharacter.visible = (options[curSelected] == 'Anti-Aliasing');
+		FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
+
+	function reloadValues() {
+		for (i in 0...checkboxArray.length) {
+			var checkbox:CheckboxThingie = checkboxArray[i];
+			if(checkbox != null) {
+				var daValue:Bool = false;
+				switch(options[checkboxNumber[i]]) {
+					case 'ModOption1':
+						daValue = ClientPrefs.modoption1;
+					case 'ModOption2':
+						daValue = ClientPrefs.modoption2;
+					case 'ModOption3':
+						daValue = ClientPrefs.modoption3;
+				}
+				checkbox.daValue = daValue;
+			}
+		}
+		for (i in 0...grpTexts.members.length) {
+			var text:AttachedText = grpTexts.members[i];
+			if(text != null) {
+				var daText:String = '';
+				switch(options[textNumber[i]]) {
+					case '':
+						daText = '' + ClientPrefs.framerate;
+					case '':
+						daText = ClientPrefs.noteOffset + 'ms';
+				}
+				var lastTracker:FlxSprite = text.sprTracker;
+				text.sprTracker = null;
+				text.changeText(daText);
+				text.sprTracker = lastTracker;
+			}
+		}
+	}
+
+	private function unselectableCheck(num:Int):Bool {
+		for (i in 0...unselectableOptions.length) {
+			if(options[num] == unselectableOptions[i]) {
+				return true;
+			}
+		}
+		return options[num] == null || options[num].length < 1;
+	} */
 }
+
+
